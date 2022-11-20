609bd0853b70101f4aabefa6596d19e229dc68b8
Minh-Khoi <rocketme144@gmail.com>
2022-11-20 18:36:19 +0700

    fix : incompleted parser text format

--- parser/imcompleted_src/parser.c
+++ parser/imcompleted_src/parser.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * @copyright (c) 2008, Hedspi, Hanoi University of Technology
  * @author Huu-Duc Nguyen
  * @version 1.0
@@ -14,21 +14,27 @@
 Token *currentToken;
 Token *lookAhead;
 
-void scan(void) {
-  Token* tmp = currentToken;
+void scan(void)
+{
+  Token *tmp = currentToken;
   currentToken = lookAhead;
   lookAhead = getValidToken();
   free(tmp);
 }
 
-void eat(TokenType tokenType) {
-  if (lookAhead->tokenType == tokenType) {
+void eat(TokenType tokenType)
+{
+  if (lookAhead->tokenType == tokenType)
+  {
     printToken(lookAhead);
     scan();
-  } else missingToken(tokenType, lookAhead->lineNo, lookAhead->colNo);
+  }
+  else
+    missingToken(tokenType, lookAhead->lineNo, lookAhead->colNo);
 }
 
-void compileProgram(void) {
+void compileProgram(void)
+{
   assert("Parsing a Program ....");
   eat(KW_PROGRAM);
   eat(TK_IDENT);
@@ -38,137 +44,169 @@ void compileProgram(void) {
   assert("Program parsed!");
 }
 
-void compileBlock(void) {
+void compileBlock(void)
+{
   assert("Parsing a Block ....");
-  if (lookAhead->tokenType == KW_CONST) {
+  if (lookAhead->tokenType == KW_CONST)
+  {
     eat(KW_CONST);
     compileConstDecl();
     compileConstDecls();
     compileBlock2();
-  } 
-  else compileBlock2();
+  }
+  else
+    compileBlock2();
   assert("Block parsed!");
 }
 
-void compileBlock2(void) {
-  if (lookAhead->tokenType == KW_TYPE) {
+void compileBlock2(void)
+{
+  if (lookAhead->tokenType == KW_TYPE)
+  {
     eat(KW_TYPE);
     compileTypeDecl();
     compileTypeDecls();
     compileBlock3();
-  } 
-  else compileBlock3();
+  }
+  else
+    compileBlock3();
 }
 
-void compileBlock3(void) {
-  if (lookAhead->tokenType == KW_VAR) {
+void compileBlock3(void)
+{
+  if (lookAhead->tokenType == KW_VAR)
+  {
     eat(KW_VAR);
     compileVarDecl();
     compileVarDecls();
     compileBlock4();
-  } 
-  else compileBlock4();
+  }
+  else
+    compileBlock4();
 }
 
-void compileBlock4(void) {
+void compileBlock4(void)
+{
   compileSubDecls();
   compileBlock5();
 }
 
-void compileBlock5(void) {
+void compileBlock5(void)
+{
   eat(KW_BEGIN);
   compileStatements();
   eat(KW_END);
 }
 
-void compileConstDecls(void) {
-while (lookAhead->tokenType==TK_IDENT) 
-compileConstDecl(); 
+void compileConstDecls(void)
+{
+  while (lookAhead->tokenType == TK_IDENT)
+    compileConstDecl();
 }
 
-void compileConstDecl(void) {
+void compileConstDecl(void)
+{
   eat(TK_IDENT);
   eat(SB_EQUAL);
   compileConstant();
   eat(SB_SEMICOLON);
 }
 
-void compileTypeDecls(void) {
+void compileTypeDecls(void)
+{
   // TODO
 }
 
-void compileTypeDecl(void) {
+void compileTypeDecl(void)
+{
   // TODO
 }
 
-void compileVarDecls(void) {
+void compileVarDecls(void)
+{
   // TODO
 }
 
-void compileVarDecl(void) {
+void compileVarDecl(void)
+{
   // TODO
 }
 
-void compileSubDecls(void) {
+void compileSubDecls(void)
+{
   assert("Parsing subtoutines ....");
   // TODO
   assert("Subtoutines parsed ....");
 }
 
-void compileFuncDecl(void) {
+void compileFuncDecl(void)
+{
   assert("Parsing a function ....");
   // TODO
   assert("Function parsed ....");
 }
 
-void compileProcDecl(void) {
+void compileProcDecl(void)
+{
   assert("Parsing a procedure ....");
   // TODO
   assert("Procedure parsed ....");
 }
 
-void compileUnsignedConstant(void) {
+void compileUnsignedConstant(void)
+{
   // TODO
 }
 
-void compileConstant(void) {
+void compileConstant(void)
+{
   // TODO
 }
 
-void compileConstant2(void) {
+void compileConstant2(void)
+{
   // TODO
 }
 
-void compileType(void) {
+void compileType(void)
+{
   // TODO
 }
 
-void compileBasicType(void) {
+void compileBasicType(void)
+{
   // TODO
 }
 
-void compileParams(void) {
+void compileParams(void)
+{
   // TODO
 }
 
-void compileParams2(void) {
+void compileParams2(void)
+{
   // TODO
 }
 
-void compileParam(void) {
+void compileParam(void)
+{
   // TODO
 }
 
-void compileStatements(void) {
+void compileStatements(void)
+{
   // TODO
 }
 
-void compileStatements2(void) {
+void compileStatements2(void)
+{
   // TODO
 }
 
-void compileStatement(void) {
-  switch (lookAhead->tokenType) {
+void compileStatement(void)
+{
+  switch (lookAhead->tokenType)
+  {
   case TK_IDENT:
     compileAssignSt();
     break;
@@ -199,100 +237,118 @@ void compileStatement(void) {
   }
 }
 
-void compileAssignSt(void) {
+void compileAssignSt(void)
+{
   assert("Parsing an assign statement ....");
   // TODO
   assert("Assign statement parsed ....");
 }
 
-void compileCallSt(void) {
+void compileCallSt(void)
+{
   assert("Parsing a call statement ....");
   // TODO
   assert("Call statement parsed ....");
 }
 
-void compileGroupSt(void) {
+void compileGroupSt(void)
+{
   assert("Parsing a group statement ....");
   // TODO
   assert("Group statement parsed ....");
 }
 
-void compileIfSt(void) {
+void compileIfSt(void)
+{
   assert("Parsing an if statement ....");
   eat(KW_IF);
   compileCondition();
   eat(KW_THEN);
   compileStatement();
-  if (lookAhead->tokenType == KW_ELSE) 
+  if (lookAhead->tokenType == KW_ELSE)
     compileElseSt();
   assert("If statement parsed ....");
 }
 
-void compileElseSt(void) {
+void compileElseSt(void)
+{
   eat(KW_ELSE);
   compileStatement();
 }
 
-void compileWhileSt(void) {
+void compileWhileSt(void)
+{
   assert("Parsing a while statement ....");
   // TODO
   assert("While statement pased ....");
 }
 
-void compileForSt(void) {
+void compileForSt(void)
+{
   assert("Parsing a for statement ....");
   // TODO
   assert("For statement parsed ....");
 }
 
-void compileArguments(void) {
+void compileArguments(void)
+{
   // TODO
 }
 
-void compileArguments2(void) {
+void compileArguments2(void)
+{
   // TODO
 }
 
-void compileCondition(void) {
+void compileCondition(void)
+{
   // TODO
 }
 
-void compileCondition2(void) {
+void compileCondition2(void)
+{
   // TODO
 }
 
-void compileExpression(void) {
+void compileExpression(void)
+{
   assert("Parsing an expression");
   // TODO
   assert("Expression parsed");
 }
 
-void compileExpression2(void) {
+void compileExpression2(void)
+{
   // TODO
 }
 
-
-void compileExpression3(void) {
+void compileExpression3(void)
+{
   // TODO
 }
 
-void compileTerm(void) {
+void compileTerm(void)
+{
   // TODO
 }
 
-void compileTerm2(void) {
+void compileTerm2(void)
+{
   // TODO
 }
 
-void compileFactor(void) {
+void compileFactor(void)
+{
   // TODO
 }
 
-void compileIndexes(void) {
+void compileIndexes(void)
+{
   // TODO
 }
 
-int compile(char *fileName) {
+int compile(char *fileName)
+{
   if (openInputStream(fileName) == IO_ERROR)
     return IO_ERROR;
 
@@ -305,5 +361,4 @@ int compile(char *fileName) {
   free(lookAhead);
   closeInputStream();
   return IO_SUCCESS;
-
 }
